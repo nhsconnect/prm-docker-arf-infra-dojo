ab6e4fdcc420b17c9d65852e1cf3b4669d74df88
Ryan Brown <ryan.brown15@nhs.net>
2023-02-15 16:38:07 +0000

    Correct java variables to work with OpenJDK 11
    Co-authored-by: Richard Smith <richard.smith33@nhs.net>

--- image/etc_dojo.d/variables/61-java-variables.sh
+++ image/etc_dojo.d/variables/61-java-variables.sh
@@ -2,6 +2,6 @@
 
 # those are set by a base image, but when using dojo, user may have overwritten
 # them, so set them again:
-export JAVA_HOME=/usr/local/openjdk-17
-export JAVA_VERSION=17.0.3
+export JAVA_HOME=/usr/local/openjdk-11
+export JAVA_VERSION=11.0.16
 export PATH=$JAVA_HOME/bin:$PATH
